dee7a04403fdde46845f445ce751dd8fadbdadd3
yibincor <424024418@qq.com>
2019-08-05 11:27:40 +0800

    临时提交

--- LanguageManage/ResxHelper/PubMethod.cs
+++ LanguageManage/ResxHelper/PubMethod.cs
@@ -1,18 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LanguagesManage.ResxHelper
 {
-    public class PubMethod
+               public class PubMethod
     {
-        public static List<string> GetRescList(string strPath)
+      
+        /// <summary>
+        /// 获取目录下的resx文件
+        /// </summary>
+        /// <param name="strPath"></param>
+        /// <returns></returns>
+        public static List<string> GetResxList(string strPath)
         {
             List<string> result = new List<string>();
+        
+            // 参数:E:\c#\LanguageManage\Login.cs
+            //1.获取文件夹路径  获取到  E:\c#\LanguageManage
+            string Temp_str = strPath.Substring(0,strPath.LastIndexOf("\\"));
 
-            return result;
-        }
+            //1.1 获取文件名称    Login.
+
+
+
+            //2.获取文件夹下的所有文件
+            DirectoryInfo root = new DirectoryInfo(Temp_str);
+            FileInfo[] files = root.GetFiles();
+
+
+            //3.遍历进行配置
+            foreach (FileInfo item in files)
+            {
+                if (item.Name.StartsWith("lo") && item.Name.EndsWith("gin")) 
+                {
+                    result.add();
+                }
+            }
+
+
+
+
+            return result; 
+        }  
     }
 }
---
+++ LanguagesManageTests1/ResxHelper/PubMethodTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LanguagesManage.ResxHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagesManage.ResxHelper.Tests
+{
+    [TestClass()]
+    public class PubMethodTests
+    {
+        [TestMethod()]
+        public void GetResxListTest()
+        {
+            string strPath = @"E:\c#\LanguageManage\Login.cs";
+            //List<string> result = PubMethod.GetResxList(strPath);
+            List<string> result = PubMethod.GetResxList(strPath);
+            Assert.AreEqual(result[0],".resx");
+        }
+    }
+}
